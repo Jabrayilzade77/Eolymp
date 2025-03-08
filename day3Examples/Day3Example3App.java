68c6fad195c70dbcc6bdc5f88c289bc18af14c6e
Jabrayilzade77 <yusif.jabrayilzade321@gmail.com>
2025-03-08 15:44:55 +0400

    Sum of odd numbers solved!

--- day3Examples/Day3Example3App.java
+++ day3Examples/Day3Example3App.java
@@ -6,10 +6,17 @@ public class Day3Example3App {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        long a = scan.nextInt();
-        long b = scan.nextInt();
-        long sum = ((b - a + 1) * (a + b)) / 2 ;
+        long a = scan.nextLong();
+        long b = scan.nextLong();
 
-        System.out.print(sum);
+        long lastOddB = (b % 2 == 0) ? (b - 1) : b;
+        long countB = (lastOddB + 1) / 2;
+        long sumB = countB * countB;
+
+        long lastOddA = ((a - 1) % 2 == 0) ? (a - 2) : (a - 1);
+        long countA = (lastOddA + 1) / 2;
+        long sumA = countA * countA;
+
+        System.out.println(sumB - sumA);
     }
 }
