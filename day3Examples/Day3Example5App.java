3b20cb2149c817bcce8ea06a0916479fd90002df
Jabrayilzade77 <yusif.jabrayilzade321@gmail.com>
2025-03-08 16:10:53 +0400

    Christmas tree solved!

---
+++ day3Examples/Day3Example5App.java
@@ -0,0 +1,22 @@
+package day3Examples;
+
+import java.util.Scanner;
+
+public class Day3Example5App {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
