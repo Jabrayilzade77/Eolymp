28dd96a9ebfc160326c3ce7de4bf4df1bf2515d3
Jabrayilzade77 <yusif.jabrayilzade321@gmail.com>
2025-03-10 16:10:54 +0400

    Quiz 4 finished!

---
+++ day5Examples/Quiz4App.java
@@ -0,0 +1,59 @@
+public class Quiz4App {
+    public static void main(String[] args) {
+
+//1. What is the output of the following code?
+//        int[] array = {4, 8, -15, 6, 5, -4, 6, -7};
+//        int temp = array[0];
+//        int num = array[0];
+//        for (int element : array) {
+//            if (element < temp) {
+//                num = temp;
+//                temp = element;
+//            } else if (element < num) {
+//                num = element;
+//            }
+//        }
+//        System.out.println(num);
+
+        //-7
+
+//2.
+//        String name = "Osman";
+//        name = name.toUpperCase();
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name);
+
+        //osman
+        //OSMAN
+
+//3. Fill in the condition of the for loop and if statement to produce the following output:
+//        int[] array = {3, 4, 8, 9, 11, 20, 13, 48, 17};
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i] % 2 == 0) {
+//                System.out.print(array[i] + " ");
+//            }
+//        }
+
+        //OUTPUT: 4 8 20 48
+
+//4.
+//        String surname = "Afandiyev";
+//        surname = surname.replace('a', 'u');//Afundiyev
+//        surname = surname.toUpperCase();//AFUNDIYEV
+//        System.out.println(surname.indexOf('V'));//8
+//        System.out.println(surname.charAt(2));//U
+
+//5. Fill in the method names and/or parameters the following output:
+//        String text = "Turing Academy";
+//        System.out.println(text.charAt(7));//7
+//        text = text.substring(6, 14);//(6, 14)
+//        System.out.println(text);
+//        boolean isSame = text.contains("Turing Academy");//contains
+//        System.out.println(isSame);
+
+        //OUTPUT:
+        //A
+        //Academy
+        //false
+    }
+}
